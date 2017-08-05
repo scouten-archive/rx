b5e5518b0287802f6ea0255022756c530c7ce4f7
Eric Scouten <eric@scouten.com>
2017-08-05 22:13:56 +0200

    Add tests for a couple of error cases in Rx.Observable.add_stage/3.

--- test/rx/observable_test.exs
+++ test/rx/observable_test.exs
@@ -2,4 +2,20 @@ defmodule Rx.ObservableTest do
   use ExUnit.Case, async: true
 
   doctest Rx.Observable
+
+  @empty_observable %Rx.Observable{} # not a supported use case!
+
+  describe "add_stage/3 (internal)" do
+    test "won't add stage to otherwise empty Observable" do
+      assert_raise RuntimeError, fn ->
+        Rx.Observable.to_notifications(@empty_observable)
+      end
+    end
+
+    test "won't add stage to a non-Observable" do
+      assert_raise RuntimeError, fn ->
+        Rx.Observable.to_notifications("not an Observable")
+      end
+    end
+  end
 end
