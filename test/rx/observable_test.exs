28845bab5c10181741c86497465e72c3f1a8ab8e
Eric Scouten <eric@scouten.com>
2017-08-05 22:44:09 +0200

    Verify that Rx.Observable's implementation of Enumerable.to_list/1 crashes if source Observable crashes.

--- test/rx/observable_test.exs
+++ test/rx/observable_test.exs
@@ -3,6 +3,8 @@ defmodule Rx.ObservableTest do
 
   doctest Rx.Observable
 
+  import ExUnit.CaptureLog
+
   @empty_observable %Rx.Observable{} # not a supported use case!
 
   describe "add_stage/3 (internal)" do
@@ -16,6 +18,19 @@ defmodule Rx.ObservableTest do
       assert_raise RuntimeError, fn ->
         Rx.Observable.to_notifications("not an Observable")
       end
+    end
+  end
+
+  describe "to_list/1 (via Enumerable)" do
+    test "crashes if source stream crashes on construction" do
+      capture_log(fn ->
+        assert {:bogus, _} = catch_exit(
+          Rx.Observable.create(fn _next ->
+            exit(:bogus)
+          end)
+          |> Enum.to_list()
+        )
+      end)
     end
   end
 end
