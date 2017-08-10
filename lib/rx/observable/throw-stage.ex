2143dd5f24922d50abf7dd5403776b9aafe80446
Eric Scouten <eric@scouten.com>
2017-08-10 09:54:03 -0700

    Cleaner implementation of Rx.Observable.throw/1.

---
+++ lib/rx/observable/throw-stage.ex
@@ -0,0 +1,21 @@
+defmodule Rx.Observable.ThrowStage do
+  @moduledoc false  # internal
+
+  use GenStage
+
+  defstruct message: nil
+
+  def start(%__MODULE__{message: message}, :producer, options \\ []), do:
+    GenStage.start(__MODULE__, {message, :producer}, options)
+
+  def init({message, :producer}), do:
+    {:producer, message}
+
+  def handle_demand(_demand, message) do
+    Process.send_after(self(), :send_stop, 10)
+    {:noreply, [], message}
+  end
+
+  def handle_info(:send_stop, message), do:
+    {:stop, {:shutdown, message}, message}
+end
