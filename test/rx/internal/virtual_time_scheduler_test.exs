93a159d469d3cf258810998b2a3637998ef8e855
Eric Scouten <eric@scouten.com>
2017-08-15 09:12:24 -0700

    Initial sketch for VirtualTimeScheduler.

---
+++ test/rx/internal/virtual_time_scheduler_test.exs
@@ -0,0 +1,98 @@
+defmodule VirtualTimeSchedulerTest do
+  use ExUnit.Case, async: true
+
+  alias VirtualTimeScheduler, as: VTS
+
+  test "can run a preconfigured sequence of events in order of addition" do
+    v = VTS.new()
+    my_ref = make_ref()
+
+    invoke = fn(0, n) ->
+      send(self(), {:invoked, my_ref, n})
+    end
+
+    v = VTS.schedule(v, 0, invoke, 1)
+    v = VTS.schedule(v, 0, invoke, 2)
+    v = VTS.schedule(v, 0, invoke, 3)
+    v = VTS.schedule(v, 0, invoke, 4)
+    v = VTS.schedule(v, 0, invoke, 5)
+    VTS.flush(v)
+
+    assert_received {:invoked, ^my_ref, n}
+    assert n == 1
+    assert_received {:invoked, ^my_ref, n}
+    assert n == 2
+    assert_received {:invoked, ^my_ref, n}
+    assert n == 3
+    assert_received {:invoked, ^my_ref, n}
+    assert n == 4
+    assert_received {:invoked, ^my_ref, n}
+    assert n == 5
+    refute_received {:invoked, ^my_ref, _n}
+  end
+
+  #   it('should schedule things in order when flushed if each this is scheduled at random', () => {
+  #     const v = new VirtualTimeScheduler();
+  #     const invoked = [];
+  #     const invoke = (state: number) => {
+  #       invoked.push(state);
+  #     };
+  #     v.schedule(invoke, 0, 1);
+  #     v.schedule(invoke, 100, 2);
+  #     v.schedule(invoke, 0, 3);
+  #     v.schedule(invoke, 500, 4);
+  #     v.schedule(invoke, 0, 5);
+  #     v.schedule(invoke, 100, 6);
+  #
+  #     v.flush();
+  #
+  #     expect(invoked).to.deep.equal([1, 3, 5, 2, 6, 4]);
+  #   });
+  #
+  #   it('should schedule things in order when there are negative delays', () => {
+  #     const v = new VirtualTimeScheduler();
+  #     const invoked = [];
+  #     const invoke = (state: number) => {
+  #       invoked.push(state);
+  #     };
+  #     v.schedule(invoke, 0, 1);
+  #     v.schedule(invoke, 100, 2);
+  #     v.schedule(invoke, 0, 3);
+  #     v.schedule(invoke, -2, 4);
+  #     v.schedule(invoke, 0, 5);
+  #     v.schedule(invoke, -10, 6);
+  #
+  #     v.flush();
+  #
+  #     expect(invoked).to.deep.equal([6, 4, 1, 3, 5, 2]);
+  #   });
+  #
+  #   it('should support recursive scheduling', () => {
+  #     const v = new VirtualTimeScheduler();
+  #     let count = 0;
+  #     const expected = [100, 200, 300];
+  #
+  #     v.schedule<string>(function(this: VirtualAction<string>, state: string) {
+  #       if (++count === 3) {
+  #         return;
+  #       }
+  #       expect(this.delay).to.equal(expected.shift());
+  #       this.schedule(state, this.delay);
+  #     }, 100, 'test');
+  #
+  #     v.flush();
+  #     expect(count).to.equal(3);
+  #   });
+  #
+  #   it('should not execute virtual actions that have been rescheduled before flush', () => {
+  #     const v = new VirtualTimeScheduler();
+  #     let messages = [];
+  #     let action: VirtualAction<string> = <VirtualAction<string>> v.schedule(function(state: string) {
+  #       messages.push(state);
+  #     }, 10, 'first message');
+  #     action = <VirtualAction<string>> action.schedule('second message' , 10);
+  #     v.flush();
+  #     expect(messages).to.deep.equal(['second message']);
+  #   });
+  # });
+end
--- test/support/virtual_time_scheduler.ex
+++ test/support/virtual_time_scheduler.ex
@@ -1,7 +1,23 @@
 defmodule VirtualTimeScheduler do
   @moduledoc false  # Only used in testing infrastructure.
 
-  defstruct pending_events: []
+  defstruct time: 0, pending_events: [], seq: 0
 
+  def new, do: %__MODULE__{}
 
+  def schedule(%__MODULE__{time: time_now, pending_events: old_events, seq: seq} = v,
+               time_delta, fun, args)
+  when time_delta >= 0 and is_function(fun, 2)
+  do
+    sched_time = time_now + time_delta
+    new_event = {sched_time, seq + 1, fun, args}
+    new_events = Enum.sort([new_event | old_events])
+    %{v | pending_events: new_events, seq: seq + 1}
+  end
+
+  def flush(%__MODULE__{pending_events: events} = _v) do
+    Enum.each(events, fn({time, _seq, fun, args}) ->
+      fun.(time, args)
+    end)
+  end
 end
