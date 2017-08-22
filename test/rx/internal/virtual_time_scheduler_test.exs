d78136111ea890f4ac2883d88aac073b28d545cd
Eric Scouten <eric@scouten.com>
2017-08-21 18:20:04 -0700

    WIP on multiplexing scheduler and Schedulable behaviour.
    Approaching parity with previous VirtualTimeScheduler.

---
+++ test/rx/internal/virtual_time_scheduler_test.exs
@@ -0,0 +1,176 @@
+defmodule VirtualTimeSchedulerTest do
+  use ExUnit.Case, async: true
+
+  alias VirtualTimeScheduler, as: VTS
+
+  # ---
+
+  defmodule SimpleSchedulable do
+    use Rx.Schedulable
+
+    defstruct placeholder: nil
+
+    def init(0, %__MODULE__{} = _) do
+      {:ok, [], new_tasks: [{0, {:append, 1}},
+                            {0, {:append, 2}},
+                            {0, {:append, 3}},
+                            {0, {:append, 4}},
+                            {0, {:append, 5}}]}
+    end
+
+    def handle_task(time, {:append, n}, acc), do: {:ok, [{time, n} | acc]}
+
+    def terminate(_time, _reason, acc), do: Enum.reverse(acc)
+  end
+
+  test "can run a preconfigured sequence of events in order of addition" do
+    assert VTS.run(%SimpleSchedulable{}) ==
+      [{0, 1}, {0, 2}, {0, 3}, {0, 4}, {0, 5}]
+  end
+
+  # ---
+
+  defmodule RandomScheduler do
+    use Rx.Schedulable
+
+    defstruct placeholder: nil
+
+    def init(0, %__MODULE__{} = _) do
+      {:ok, [], new_tasks: [{0, {:append, 1}},
+                            {100, {:append, 2}},
+                            {0, {:append, 3}},
+                            {500, {:append, 4}},
+                            {0, {:append, 5}},
+                            {100, {:append, 6}}]}
+    end
+
+    def handle_task(time, {:append, n}, acc), do: {:ok, [{time, n} | acc]}
+
+    def terminate(_time, _reason, acc), do: Enum.reverse(acc)
+  end
+
+  test "can run tasks in order sorted by time, even if scheduled at random" do
+    assert VTS.run(%RandomScheduler{}) ==
+      [{0, 1}, {0, 3}, {0, 5}, {100, 2}, {100, 6}, {500, 4}]
+  end
+
+  # ---
+
+  defmodule ReversingScheduler do
+    use Rx.Schedulable
+
+    defstruct placeholder: nil
+
+    def init(0, %__MODULE__{} = _) do
+      {:ok, [], new_tasks: [{0, {:append, 1}},
+                            {100, {:append, 2}},
+                            {0, {:append, 3}},
+                            {500, {:append, 4}},
+                            {0, {:append, 5}},
+                            {100, {:append, 6}},
+                            {700, :reverse}]}
+    end
+
+    def handle_task(time, {:append, n}, acc), do: {:ok, [{time, n} | acc]}
+    def handle_task(_time, :reverse, acc), do: {:ok, Enum.reverse(acc)}
+
+    def terminate(_time, _reason, acc), do: acc
+  end
+
+  test "can run tasks that call different functions" do
+    assert VTS.run(%ReversingScheduler{}) ==
+      [{0, 1}, {0, 3}, {0, 5}, {100, 2}, {100, 6}, {500, 4}]
+  end
+
+  # ---
+
+  defmodule BackwardsScheduler do
+    use Rx.Schedulable
+
+    defstruct placeholder: nil
+
+    def init(0, _), do: {:ok, [], new_tasks: [{-10, :whatever}]}
+    def handle_task(_time, _, acc), do: {:ok, acc}
+    def terminate(_time, _reason, acc), do: acc
+  end
+
+  test "does not accept negative delays" do
+    # NOTE: This differs from RxJS implementation. Maybe we'll have to revisit?
+
+    assert_raise ArgumentError, fn ->
+      VTS.run(%BackwardsScheduler{})
+    end
+  end
+
+  # ---
+
+  defmodule RecursiveScheduler do
+    use Rx.Schedulable
+
+    defstruct placeholder: nil
+
+    def init(0, _), do: {:ok, [], new_tasks: [{0, {:append, 1}}]}
+
+    def handle_task(0, {:append, n}, acc) when n > 4, do: {:ok, acc}
+    def handle_task(0, {:append, n}, acc), do:
+      {:ok, [{0, n} | acc], new_tasks: [{0, {:append, n + 1}}]}
+
+    def terminate(_time, _reason, acc), do: Enum.reverse(acc)
+  end
+
+  test "can schedule new events at same 'time' while running" do
+    assert VTS.run(%RecursiveScheduler{}) ==
+      [{0, 1}, {0, 2}, {0, 3}, {0, 4}]
+  end
+
+  # ---
+
+  defmodule RecursiveSchedulerWithDelay do
+    use Rx.Schedulable
+
+    defstruct placeholder: nil
+
+    def init(0, _), do: {:ok, [], new_tasks: [{10, {:append, 1}}]}
+
+    def handle_task(_time, {:append, n}, acc) when n > 4, do: {:ok, acc}
+    def handle_task(time, {:append, n}, acc), do:
+      {:ok, [{time, n} | acc], new_tasks: [{10, {:append, n + 1}}]}
+
+    def terminate(_time, _reason, acc), do: Enum.reverse(acc)
+  end
+
+  test "can schedule new events at later 'time' while running" do
+    assert VTS.run(%RecursiveSchedulerWithDelay{}) ==
+      [{10, 1}, {20, 2}, {30, 3}, {40, 4}]
+  end
+
+  # ---
+
+  defmodule StatefulSchedulable do
+    use Rx.Schedulable
+
+    defstruct starting_count: 0
+
+    def init(0, %__MODULE__{starting_count: starting_count}),
+      do: {:ok, [], new_tasks: [{10, {:append, starting_count}}]}
+
+    def handle_task(_time, {:append, n}, acc) when n > 45, do: {:ok, acc}
+    def handle_task(time, {:append, n}, acc), do:
+      {:ok, [{time, n} | acc], new_tasks: [{10, {:append, n + 1}}]}
+
+    def terminate(_time, _reason, acc), do: Enum.reverse(acc)
+  end
+
+  test "passes arguments through from original struct to init func" do
+    assert VTS.run(%StatefulSchedulable{starting_count: 42}) ==
+      [{10, 42}, {20, 43}, {30, 44}, {40, 45}]
+  end
+
+  # ---
+
+  # test "maintains distinction among distinct schedulable structs"
+
+  # test "passes correct time to init function for secondary object"
+
+  # test "can pass messages from one schedulable struct to another"
+end
---
+++ test/support/virtual_time_scheduler.ex
@@ -0,0 +1,117 @@
+defmodule VirtualTimeScheduler do
+  @moduledoc false  # Only used in testing infrastructure.
+
+  defstruct pending_tasks: [],
+            task_seq: 0,
+            modules: %{},
+            sub_states: %{},
+            schedulable_seq: 0,
+            terminate_results: %{}
+
+  def run(%{__struct__: _module} = first_schedulable) do
+    %__MODULE__{}
+    |> schedule_init(0, first_schedulable)
+    |> schedule_terminate(:done, 1, :normal)
+    |> run_tasks()
+  end
+
+  defp run_tasks(%__MODULE__{pending_tasks: []} = v), do: v.terminate_results[1]
+  defp run_tasks(%__MODULE__{pending_tasks: [current_task | remaining_tasks]} = v) do
+    v = %{v | pending_tasks: remaining_tasks}
+
+    v
+    |> run_task(current_task)
+    |> handle_options(v)
+    |> run_tasks()
+  end
+
+  defp run_task(%__MODULE__{},
+                {time, _seq, sid, {:init, %{__struct__: module} = schedulable}})
+  do
+    {:init, time, sid, module.init(time, schedulable)}
+  end
+
+  defp run_task(%__MODULE__{modules: modules, sub_states: sub_states},
+                {time, _seq, sid, {:task, task}})
+  do
+    module = Map.get(modules, sid)
+    sub_state = Map.get(sub_states, sid)
+    {:task, time, sid, module.handle_task(time, task, sub_state )}
+  end
+
+  defp run_task(%__MODULE__{modules: modules, sub_states: sub_states},
+                {time, _seq, sid, {:terminate, reason}})
+  do
+    module = Map.get(modules, sid)
+    sub_state = Map.get(sub_states, sid)
+
+    case module do
+      nil -> :nop
+      _ -> {:terminate, time, sid, module.terminate(time, reason, sub_state)}
+    end
+  end
+
+  defp handle_options(:nop, v), do: v
+
+  defp handle_options({:terminate, _time, sid, terminate_results}, v) do
+    %{v | sub_states: Map.delete(v.sub_states, sid),
+          modules: Map.delete(v.modules, sid),
+          terminate_results: Map.put(v.terminate_results, sid, terminate_results)}
+  end
+
+  defp handle_options({callback, time, sid, {:ok, sub_state}}, v) do
+    handle_options({callback, time, sid, {:ok, sub_state, []}}, v)
+  end
+
+  defp handle_options({_callback, time, sid, {:ok, sub_state, options}}, v) do
+    v
+    |> put_sub_state(sid, sub_state)
+    |> add_tasks(time, sid, Keyword.get(options, :new_tasks, []))
+  end
+
+  defp put_sub_state(%__MODULE__{sub_states: sub_states} = v,
+                     sid, sub_state)
+  do
+    %{v | sub_states: Map.put(sub_states, sid, sub_state)}
+  end
+
+  defp add_tasks(v, _time, _sid, []), do: v
+
+  defp add_tasks(v, time, sid, new_tasks) do
+    validate_tasks(new_tasks)
+
+    Enum.reduce(new_tasks, v, fn({time_delta, task}, acc) ->
+      add_task(acc, time + time_delta, sid, {:task, task})
+    end)
+  end
+
+  defp validate_tasks(tasks) do
+    unless Enum.all?(tasks, &validate_task/1), do:
+      raise ArgumentError, "invalid task passed to VTS run callback\n#{inspect tasks}"
+
+    tasks
+  end
+
+  defp validate_task({time, _task}) when is_integer(time) and time >= 0, do: true
+  defp validate_task(_), do: false
+
+  defp schedule_init(%__MODULE__{schedulable_seq: sseq, modules: modules} = v,
+                     time_now,
+                     %{__struct__: module} = schedulable)
+  do
+    add_task(%{v | schedulable_seq: sseq + 1,
+                   modules: Map.put(modules, sseq + 1, module)},
+             time_now, sseq + 1, {:init, schedulable})
+  end
+
+  defp schedule_terminate(%__MODULE__{} = v, time, sid, reason) do
+    add_task(v, time, sid, {:terminate, reason})
+  end
+
+  defp add_task(%__MODULE__{pending_tasks: old_tasks, task_seq: seq} = v,
+                time, sid, task)
+  do
+    new_task = {time, seq + 1, sid, task}
+    %{v | pending_tasks: Enum.sort([new_task | old_tasks]), task_seq: seq + 1}
+  end
+end
