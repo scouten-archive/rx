9f566ce8f14dd0e2415a5cbc4cb71c00522e6cb6
Eric Scouten <eric@scouten.com>
2017-08-22 22:31:11 -0700

    Retrofit MarbleTesting.ColdObservable to use new Schedulable interface.
    Break out MarbleTesting.Observer into its own module.

---
+++ test/support/marble_testing/observer.ex
@@ -0,0 +1,24 @@
+defmodule MarbleTesting.Observer do
+  @moduledoc false
+
+  use Rx.Schedulable
+
+  defstruct [:observable]
+
+  def init(_time, %__MODULE__{observable: source} = _), do:
+    {:ok, [], start: [{0, :marble_source, source}]}
+
+  def handle_task(time, {:next, values}, acc) do
+    new_notifs =
+      values
+      |> Enum.reverse()
+      |> Enum.map(&({time, :next, &1}))
+
+    {:ok, new_notifs ++ acc}
+  end
+
+  def handle_task(time, :done, acc), do:
+    {:ok, [{time, :done} | acc], stop: [{:marble_source, :done}]}
+
+  def terminate(_time, _reason, acc), do: Enum.reverse(acc)
+end
